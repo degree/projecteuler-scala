e2d80f16886ec7b76b9ef7198ccb2b5fbfabdeef
Eugene Dubrovka <eugene.dubrovka@gmail.com>
2011-11-07 12:39:01 +0300

    Euler 19 is solved

---
+++ src/main/scala/app/Euler19.scala
@@ -0,0 +1,29 @@
+package app
+
+/**
+ * User: Eugene Dubrovka
+ * Date: 11/6/11
+ * Time: 4:15 PM
+ */
+
+object Euler19 extends App
+{
+	implicit def monthToFunc(n: Int) = (_: Int) => n
+
+	val months = List[(Int) => Int](
+		31, (year: Int) => if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)) 29 else 28, 31, // jan, feb, mar
+		30, 31, 30, // apr, may, jun
+		31, 31, 30, // jul, aug, sep
+		31, 30, 31  // oct, nov, dec
+	)
+
+	def count(y1: Int, y2: Int) = {
+		val rangeYears = Range(y1, y2 + 1)
+		val firstDaysOfMonthsInYearRange = 1 :: (rangeYears.flatMap(y => months.map(f => (f(y) % 7)))).toList
+		val tuple = ((List[Int](), 0) /: firstDaysOfMonthsInYearRange) {(t, i) => {val x = t._2 + i; (x :: t._1, x)}}
+		tuple._1.reverse.map(_ % 7).count(_ == 0)
+	}
+
+	println(count(1900, 2001) - count(1900, 1901))
+
+}
