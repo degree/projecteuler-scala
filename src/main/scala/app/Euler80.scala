842e004fb6e77ae42d77d064409341819b70a5eb
Eugene Dubrovka <eugene.dubrovka@gmail.com>
2011-11-11 01:31:20 +0300

    Euler80: refactored solution

--- src/main/scala/app/Euler80.scala
+++ src/main/scala/app/Euler80.scala
@@ -4,47 +4,44 @@ import annotation.tailrec
 
 /**
  * User: Eugene Dubrovka
- * Date: 11/10/11
- * Time: 4:09 PM
+ * Date: 11/11/11
+ * Time: 01:15
  */
 
 object Euler80 extends App
 {
-	val N = 20
+	val N = 100
+	val digitsReverse = 9 to 0 by -1
+	val rationals = (1 to 10) map {x => x * x}
 
-	def sumDigits(n: Int): Int =
+	/**
+	 * This formula will work only for integer numbers in range from 1 to 99.
+	 * It is enough for this task
+	 */
+	def sumDigitsInRoot(N: Int)(n: Int) =
 	{
 		@tailrec
-		def digitInternal(sum: Int, p: BigInt, r: BigInt, count: Int, first: Boolean): Int =
+		def digitInternal(sum: Int, root: BigInt, rem: BigInt, count: Int, append: Int = 0): Int =
 		{
+			@inline
+			def y(d: Int, r: BigInt) = ((20 * d) * r) + (d * d)
+
 			if (count > 0)
 			{
-				val c: BigInt = r * 100 + (if (first) n else 0)
+				val c = rem * 100 + append
+				val d = (digitsReverse find {y(_, root) <= c}).get
 
-				var digit = -1
-				var y: BigInt = 0
-				do
-				{
-					digit += 1
-					y = (20 * p + digit) * digit
-				} while (y <= c)
-
-				digit -= 1
-				y = (20 * p + digit) * digit
-
-				print(digit)
-				digitInternal(sum + digit, p * 10 + digit, c - y, count - 1, false)
+				digitInternal(sum + d, root * 10 + d, c - y(d, root), count - 1)
 			}
-			else
-			{
-				println()
-				sum
-			}
+			else sum
 		}
 
-		digitInternal(0, 0, 0, N, true)
+		digitInternal(0, 0, 0, N, n)
 	}
 
-	println("141421356237309504880")
-	println(sumDigits(2))
+	val sumNdigits = sumDigitsInRoot(N) _
+
+	println(
+		(0 /: (1 to 100)) {(s, n) => s + (if (!(rationals contains n)) sumNdigits(n) else 0)}
+	)
 }
