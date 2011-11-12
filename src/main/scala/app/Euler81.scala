d9de860290910c448e6268adf36809577edcb00c
Eugene Dubrovka <eugene.dubrovka@gmail.com>
2011-11-12 16:56:10 +0300

    Euler 81 removed comments

--- src/main/scala/app/Euler81.scala
+++ src/main/scala/app/Euler81.scala
@@ -8,9 +8,9 @@ import io.Source
  * Time: 3:10 PM
  */
 
+// NOTE: this is not a pretty solution :(
 object Euler81 extends App
 {
-//	val filename = "/task_81_test.txt"
 	val filename = "/task_81.txt"
 
 	val lines = Source.fromInputStream(getClass.getResourceAsStream(filename)).getLines().map(_.trim.split(",").map(_.toInt)).toArray
@@ -24,35 +24,22 @@ object Euler81 extends App
 
 	for (row <- 0 until size; r <- row to 0 by -1; val c = row - r)
 	{
-//		print((row, c))
-//		print(" -> ")
-//		println((r, c))
 		matrix(row)(c) = lines(r)(c)
 	}
 
 	for (row <- size until height; val cells = height - row; c <- 0 until cells; val r = size - c - 1)
 	{
-//		print((row, c))
-//		print(" -> ")
-//		println((r, row - r))
 		matrix(row)(c) = lines(r)(row - r)
 	}
-
-//	matrix.foreach{arr => arr.foreach{x =>  print(x formatted "%10d")}; println()}
-//	println("================================================================")
 
 	for (r <- (height - 1) to size by -1; c <- 0 to (height - r))
 	{
 		matrix(r - 1)(c) += math.min(matrix(r)(c), matrix(r)(math.max(0, c - 1)))
-//		matrix.foreach{arr => arr.foreach{x =>  print(x formatted "%10d")}; println()}
-//		println("================================================================")
 	}
 
 	for (r <- (size - 2) to 0 by -1; c <- 0 to r)
 	{
 		matrix(r)(c) += math.min(matrix(r + 1)(c), matrix(r + 1)(math.min(c + 1, size - 1)))
-//		matrix.foreach{arr => arr.foreach{x =>  print(x formatted "%10d")}; println()}
-//		println("================================================================")
 	}
 
 	println(matrix(0)(0))
